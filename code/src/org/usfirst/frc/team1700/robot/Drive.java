2d93e9d73014622db0e64a459e61439428d327e5
Cantevisa <18vwoo@castilleja.org>
2017-01-15 11:26:03 -0800

    Added current readings to drive.

--- code/src/org/usfirst/frc/team1700/robot/Drive.java
+++ code/src/org/usfirst/frc/team1700/robot/Drive.java
@@ -2,17 +2,21 @@ package org.usfirst.frc.team1700.robot;
 
 import com.ctre.CANTalon;
 
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
+
 public class Drive {
 	CANTalon rightFront;
     CANTalon rightBack;
     CANTalon leftFront;
     CANTalon leftBack;
+    PowerDistributionPanel pdp;
 	
 	public Drive() {
 		rightFront = new CANTalon(0); //TODO: Change ID's
 		rightBack = new CANTalon(1);
 		leftFront = new CANTalon(2);
 		leftBack = new CANTalon(3);
+		pdp = new PowerDistributionPanel(4);
 	}
 	
 	public void driveTank(double leftSpeed, double rightSpeed) {
@@ -20,5 +24,9 @@ public class Drive {
 		leftBack.set(leftSpeed);
 		rightFront.set(rightSpeed);
 		rightBack.set(rightSpeed);
+		System.out.println("Right front motor current: " + pdp.getCurrent(0));
+		System.out.println("Right back motor current: " + pdp.getCurrent(1));
+		System.out.println("Left front motor current: " + pdp.getCurrent(2));
+		System.out.println("Left back motor current: " + pdp.getCurrent(3));
 	}
 }
