bad79526b69d8769442e12d1e2273c673513989e
Christine Cho <17ccho@castilleja.org>
2017-02-18 09:58:44 -0800

    Adjusted low goal sensors.

--- code/src/org/usfirst/frc/team1700/robot/LowGoal.java
+++ code/src/org/usfirst/frc/team1700/robot/LowGoal.java
@@ -6,67 +6,42 @@ import edu.wpi.first.wpilibj.Ultrasonic;
 import edu.wpi.first.wpilibj.Victor;
 
 public class LowGoal {
-	Victor firstRampMotor;
-	Victor secondRampMotor;
-	DigitalInput topSensor;
+	Victor highRampMotor;
+	Victor lowRampMotor;
+	DigitalInput highTopSensor;
+	DigitalInput lowTopSensor;
 	DigitalInput bottomSensor;
-	AnalogInput depthSensor;
 	
 	public LowGoal() {
-		firstRampMotor = new Victor(Constants.PWM.FIRST_DUMPER_MOTOR.getPort());
-		secondRampMotor = new Victor(Constants.PWM.SECOND_DUMPER_MOTOR.getPort());
-		topSensor = new DigitalInput(Constants.DigitalIO.TOP_SENSOR_ID.getPort()); 
-		depthSensor = new AnalogInput(Constants.AnalogInput.RAMP_RANGEFINDER.getPort());
+		highRampMotor = new Victor(Constants.PWM.FIRST_DUMPER_MOTOR.getPort());
+		lowRampMotor = new Victor(Constants.PWM.SECOND_DUMPER_MOTOR.getPort());
+		highTopSensor = new DigitalInput(Constants.DigitalIO.HIGH_TOP_RAMP_ID.getPort());
+		lowTopSensor = new DigitalInput(Constants.DigitalIO.LOW_TOP_RAMP_ID.getPort());
 		bottomSensor = new DigitalInput(Constants.DigitalIO.BOTTOM_SENSOR_ID.getPort());
 	}
 	
-	public int getRampHeight() {
-		int distance = depthSensor.getValue();
-		return distance;
-	}
 	
 	public void moveUp() {
-		if (!topSensor.get()) {
-			System.out.println("Limit Switch: " + topSensor.get());
-			if (getRampHeight() > Constants.RAMP_MOVEMENT_VERTICAL_DIST*Constants.RAMP_SLOW_DOWN_POINT) {
-				firstRampMotor.set(Constants.Values.Drive.MID_RAMP_SPEED);
-				secondRampMotor.set(Constants.Values.Drive.MID_RAMP_SPEED);
-			}
-			firstRampMotor.set(1);
-			secondRampMotor.set(1);
+		if(!highTopSensor.get()) {
+			highRampMotor.set(0.6);
+		} else {
+			highRampMotor.set(0);
 		}
-		else {
-			firstRampMotor.set(0);
-			secondRampMotor.set(0);
+		if(!lowTopSensor.get()) {
+			lowRampMotor.set(0.6);
+		} else {
+			lowRampMotor.set(0);
 		}
 	}
 	
 		
 	public void moveDown() {
 		if (!bottomSensor.get()) {
-			if (getRampHeight() < Constants.RAMP_MOVEMENT_VERTICAL_DIST*Constants.RAMP_SLOW_DOWN_POINT) {
-				firstRampMotor.set(-Constants.Values.Drive.MID_RAMP_SPEED);
-				secondRampMotor.set(-Constants.Values.Drive.MID_RAMP_SPEED);
-			}
-			firstRampMotor.set(-1);
-			secondRampMotor.set(-1);
+			highRampMotor.set(0.7);
+			lowRampMotor.set(0.4);
 		} else {
-			firstRampMotor.set(0);
-			secondRampMotor.set(0);
+			highRampMotor.set(0);
+			lowRampMotor.set(0);
 		}
 	}
-	
-	public void moveRampManual(double speed) {
-		if (!topSensor.get() && speed < 0) {
-			firstRampMotor.set(speed);
-			secondRampMotor.set(speed);
-		} else if (!bottomSensor.get() && speed > 0) {
-			firstRampMotor.set(speed);
-			secondRampMotor.set(speed);
-		} else {
-			firstRampMotor.set(0);
-			secondRampMotor.set(0);
-		}
-	}
-	
 }
